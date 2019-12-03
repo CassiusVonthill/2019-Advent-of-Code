ddcf6d1708ff0026f5c56a1bf38d92229c20b9ff
Cassius Vonthill <cassiusvonthill@gmail.com>
2019-12-03 08:49:23 -0500

    :sparkles: Elixir day 1

---
+++ CassiusVonthill-Elixir/day-1/day-1-test.exs
@@ -0,0 +1,40 @@
+Code.load_file("day-1.exs", __DIR__)
+
+ExUnit.start()
+ExUnit.configure(exclude: :pending, trace: true)
+
+defmodule Day1Test do
+  use ExUnit.Case
+
+  test "mass of 12" do
+    assert Day1.required_fuel(12) == 2
+  end
+
+  test "mass of 14" do
+    assert Day1.required_fuel(14) == 2
+  end
+
+  test "mass of 1969" do
+    assert Day1.required_fuel(1969) == 654
+  end
+
+  test "mass of 100756" do
+    assert Day1.required_fuel(100_756) == 33583
+  end
+
+  test "fuel for masses of 12 and 14" do
+    assert Day1.part1([12, 14]) == 4
+  end
+
+  test "fuel for fuel of mass 14" do
+    assert Day1.fuel_required_fuel_recur(14) == 16
+  end
+
+  test "fuel for fuel of mass 654" do
+    assert Day1.fuel_required_fuel_recur(654) == 966
+  end
+
+  test "fuel for fuel of mass 33583" do
+    assert Day1.fuel_required_fuel_recur(33583) == 50346
+  end
+end
---
+++ CassiusVonthill-Elixir/day-1/day-1.exs
@@ -0,0 +1,45 @@
+defmodule Day1 do
+  def load_file(path) when is_binary(path) do
+    with {:ok, content} <- File.read(path),
+         masses <- String.split(content, "\n") do
+      masses
+      |> Enum.map(&String.to_integer/1)
+    else
+      _ -> IO.puts("Something odd happened")
+    end
+  end
+
+  def part1(masses) when is_list(masses) do
+    Task.async_stream(masses, Day1, :required_fuel, [])
+    |> Stream.map(fn {:ok, v} -> v end)
+    |> Enum.sum()
+  end
+
+  def required_fuel(mass) when is_number(mass) do
+    mass
+    |> div(3)
+    |> (&(&1 - 2)).()
+  end
+
+  def part2(masses) when is_list(masses) do
+    masses
+    |> Task.async_stream(fn module_mass ->
+      module_mass
+      |> required_fuel
+      |> fuel_required_fuel_recur
+    end)
+    |> Stream.map(fn {:ok, v} -> v end)
+    |> Enum.sum()
+  end
+
+  def fuel_required_fuel_recur(mass), do: fuel_required_fuel_recur(mass, mass)
+
+  def fuel_required_fuel_recur(mass, acc) when is_number(mass) do
+    mass
+    |> required_fuel
+    |> case do
+      new_mass when new_mass <= 0 -> acc
+      new_mass -> fuel_required_fuel_recur(new_mass, acc + new_mass)
+    end
+  end
+end
