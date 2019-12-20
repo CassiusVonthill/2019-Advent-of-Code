9d69556cd258bc1714b9b8f5b33d8296fc28afb8
Cassius Vonthill <cassiusvonthill@gmail.com>
2019-12-20 09:39:37 -0500

    :sparkles: Elixir Day 6

--- CassiusVonthill-Elixir/lib/advent_of_code2019/day6.ex
+++ CassiusVonthill-Elixir/lib/advent_of_code2019/day6.ex
@@ -1,32 +1,47 @@
 defmodule AdventOfCode2019.Day6 do
-  def load_file(path) do
-    for line <- File.stream!(path) do
-      line
-      |> String.split(")", trim: true)
-      |> List.to_tuple()
-    end
+  # Solution blatantly stolen from /u/superpangolinseed
+  # from the solution megathread with structural changes
+
+  # I spent a considerable amount of hours trying to implement something
+  # using trees / graphs and actively avoiding walking around a map.
+
+  # This implementation closely mirrors what I would have done which is why
+  # I'm taking it.
+  # However, I thought the code to be made more readable and "elixir-like"
+
+  def part1(graph) when is_map(graph) do
+    graph
+    |> Map.keys()
+    |> Enum.flat_map(fn object -> orbits(graph, object) end)
+    |> Enum.count()
   end
 
-  defp _gen_tree(input) when is_list(input) do
-    com = NaryTree.Node.new("COM")
-    tree = NaryTree.new(com)
-    name_to_id = %{"COM" => com.id}
+  def part2(graph) when is_map(graph) do
+    [you, san] =
+      ["YOU", "SAN"]
+      |> Stream.map(&orbits(graph, &1))
+      |> Stream.map(&Enum.with_index/1)
+      |> Enum.map(&Map.new(&1))
 
-    input
-    |> Enum.reduce({tree, name_to_id}, fn {parent, child}, _acc = {tree, name_to_id} ->
-      child_node = NaryTree.Node.new(child)
-      parent_id = name_to_id[parent]
+    [you, san]
+    |> Stream.map(&Map.keys/1)
+    |> Enum.map(&MapSet.new/1)
+    |> (fn [x, y] -> MapSet.intersection(x, y) end).()
+    |> Enum.map(fn key -> Map.get(you, key) + Map.get(san, key) end)
+    |> Enum.min()
+  end
 
-      {NaryTree.add_child(tree, child_node, parent_id),
-       Map.put_new(name_to_id, child, child_node.id)}
+  defp orbits(g, object), do: orbits(g, object, []) |> Enum.reverse() |> Enum.drop(1)
+  defp orbits(_, nil, found), do: found
+  defp orbits(g, object, found), do: orbits(g, Map.get(g, object), [object | found])
+
+  def load_file(path) when is_binary(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{}, fn line, g ->
+      [to, from] = String.split(line, ")")
+      Map.put(g, from, to)
     end)
-    |> elem(0)
-  end
-
-  def part1(diagram) when is_list(diagram) do
-    diagram
-    |> _gen_tree()
-    |> NaryTree.to_list()
-    |> Enum.reduce(0, &(&1.level + &2))
   end
 end
--- CassiusVonthill-Elixir/test/day6_test.exs
+++ CassiusVonthill-Elixir/test/day6_test.exs
@@ -4,11 +4,33 @@ defmodule Day6Test do
 
   @prompt "test/d6/prompt.txt"
   @example1 "test/d6/e1.txt"
+  @example2 "test/d6/e2.txt"
 
   test "Example 1 orbit count" do
     @example1
     |> Day6.load_file()
     |> Day6.part1()
     |> (&(&1 == 42)).()
+  end
+
+  test "Prompt orbit count" do
+    @prompt
+    |> Day6.load_file()
+    |> Day6.part1()
+    |> (&(&1 == 417_916)).()
+  end
+
+  test "Example 2 orbit transfers" do
+    @example2
+    |> Day6.load_file()
+    |> Day6.part2()
+    |> (&(&1 == 4)).()
+  end
+
+  test "Prompt orbit transfers" do
+    @prompt
+    |> Day6.load_file()
+    |> Day6.part2()
+    |> (&(&1 == 523)).()
   end
 end
