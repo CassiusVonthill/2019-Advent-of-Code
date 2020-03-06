22a64e554a5e62c68ae7ee1a8bb954b66763cebf
Cassius Vonthill <cassiusvonthill@gmail.com>
2020-03-06 12:40:37 -0500

    :sparkles: Day 14

--- CassiusVonthill-Elixir/lib/advent_of_code2019/day14.ex
+++ CassiusVonthill-Elixir/lib/advent_of_code2019/day14.ex
@@ -1,4 +1,78 @@
 defmodule AdventOfCode2019.Day14 do
+  defmodule Reaction do
+    @enforce_keys [:reference, :cache]
+    defstruct [:reference, :cache]
+
+    def new(reference, cache \\ %{}) when is_map(reference) and is_map(cache) do
+      %Reaction{reference: reference, cache: cache}
+    end
+
+    defp calculate_required(reference, {element, held_amt}) do
+      {unit_amt, deps} = Map.fetch!(reference, element)
+
+      multiplier = ceil(held_amt / unit_amt)
+      created_amt = multiplier * unit_amt
+
+      case multiplier do
+        1 -> deps
+        n -> Enum.map(deps, fn {k, v} -> {k, v * n} end)
+      end
+      |> Map.new()
+      |> Map.put(element, -1 * created_amt)
+    end
+
+    # def to_ore(%Reaction{cache: cache = %{ORE: v}}) when map_size(cache) == 1, do: v
+
+    def to_ore(reaction = %Reaction{cache: cache, reference: reference}) do
+      queue =
+        cache
+        |> Stream.reject(fn {element, _value} -> element == :ORE end)
+        |> Enum.filter(fn {_element, cnt} -> cnt > 0 end)
+
+      if not Enum.empty?(queue) do
+        new_cache =
+          queue
+          |> Stream.map(&calculate_required(reference, &1))
+          |> Enum.reduce(cache, &Map.merge(&1, &2, fn _k, v1, v2 -> v1 + v2 end))
+
+        to_ore(%{reaction | cache: new_cache})
+      else
+        Map.fetch!(cache, :ORE)
+      end
+    end
+
+    defp binary_search(_reference, low, high, _target) when high - low <= 1 do
+      low
+    end
+
+    defp binary_search(reference, low, high, target) do
+      pivot = div(low + high, 2)
+
+      ore_needed =
+        reference
+        |> Reaction.new(%{FUEL: pivot})
+        |> Reaction.to_ore()
+
+      if ore_needed > target do
+        binary_search(reference, low, pivot, target)
+      else
+        binary_search(reference, pivot, high, target)
+      end
+    end
+
+    def optimal_fuel(reference, max_ore) do
+      cost_one = Reaction.to_ore(%Reaction{reference: reference, cache: %{FUEL: 1}})
+      low = div(max_ore, cost_one)
+      high = low * 2
+
+      # IO.inspect(cost_one, label: "Cost One: ")
+      # IO.inspect(low, label: "Low: ")
+      # IO.inspect(high, label: "High: ")
+
+      binary_search(reference, low, high, max_ore)
+    end
+  end
+
   def load_file(path) when is_binary(path) do
     for line <- File.stream!(path) do
       line
@@ -9,10 +83,20 @@ defmodule AdventOfCode2019.Day14 do
         |> String.split()
         |> List.to_tuple()
       end)
-      |> Stream.map(fn {amt, type} -> {String.to_integer(amt), String.to_atom(type)} end)
+      |> Stream.map(fn {amt, type} -> {String.to_atom(type), String.to_integer(amt)} end)
       |> Enum.split(-1)
-      |> (fn {ingredients, [{amt, type}]} -> {type, {amt, ingredients}} end).()
+      |> (fn {ingredients, [{type, amt}]} -> {type, {amt, Map.new(ingredients)}} end).()
     end
     |> Map.new()
+  end
+
+  def part1(reference) do
+    reference
+    |> Reaction.new(%{FUEL: 1})
+    |> Reaction.to_ore()
+  end
+
+  def part2(reference, max_ore \\ 1_000_000_000_000) do
+    Reaction.optimal_fuel(reference, max_ore)
   end
 end
--- CassiusVonthill-Elixir/test/advent_of_code2019/day14_test.exs
+++ CassiusVonthill-Elixir/test/advent_of_code2019/day14_test.exs
@@ -4,13 +4,95 @@ defmodule Day14Test do
 
   @prompt "test/d14/prompt.txt"
   @example1 "test/d14/e1.txt"
+  @example2 "test/d14/e2.txt"
+  @example3 "test/d14/e3.txt"
+  @example4 "test/d14/e4.txt"
 
   test "example 1 file loading" do
     @example1
     |> Day14.load_file()
-    |> (&assert(%{FUEL: {1, [{2, :AB}, {3, :BC}, {4, :CA}]}} = &1)).()
+    |> Map.fetch!(:FUEL)
+    |> (&assert({1, %{AB: 2, BC: 3, CA: 4}} = &1)).()
   end
 
   test "example 1 calculating ORE" do
+    result =
+      @example1
+      |> Day14.load_file()
+      |> Day14.part1()
+
+    assert result == 165
+  end
+
+  test "example 2 calculating ORE" do
+    result =
+      @example2
+      |> Day14.load_file()
+      |> Day14.part1()
+
+    assert result == 13312
+  end
+
+  test "example 3 calculating ORE" do
+    result =
+      @example3
+      |> Day14.load_file()
+      |> Day14.part1()
+
+    assert result == 180_697
+  end
+
+  test "example 4 calculating ORE" do
+    result =
+      @example4
+      |> Day14.load_file()
+      |> Day14.part1()
+
+    assert result == 2_210_736
+  end
+
+  test "prompt calculating ORE" do
+    result =
+      @prompt
+      |> Day14.load_file()
+      |> Day14.part1()
+
+    assert result == 362_713
+  end
+
+  test "example 2 max fuel" do
+    result =
+      @example2
+      |> Day14.load_file()
+      |> Day14.part2()
+
+    assert result == 82_892_753
+  end
+
+  test "example 3 max fuel" do
+    result =
+      @example3
+      |> Day14.load_file()
+      |> Day14.part2()
+
+    assert result == 5_586_022
+  end
+
+  test "example 4 max fuel" do
+    result =
+      @example4
+      |> Day14.load_file()
+      |> Day14.part2()
+
+    assert result == 460_664
+  end
+
+  test "prompt max fuel" do
+    result =
+      @prompt
+      |> Day14.load_file()
+      |> Day14.part2()
+
+    assert result == 3_281_820
   end
 end
